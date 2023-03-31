91f66387f23aab41d0aadea25f222111729f7850
KimWooHyun <ktkwhms3@gmail.com>
2023-03-31 19:35:11 +0900

    Chore:  add babel-eslint, eslint 파싱 에러 fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+var disabledCss = {
+  'code::before': false,
+  'code::after': false,
+  'blockquote p:first-of-type::before': false,
+  'blockquote p:last-of-type::after': false,
+  pre: false,
+  code: false,
+  'pre code': false,
+  'code::before': false,
+  'code::after': false,
+};
+
 module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}', // Note the addition of the `app` directory.
@@ -9,6 +22,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        grey: {
+          100: '#F5F7FA',
+          1000: '#1F2933',
+        },
+      },
       fontFamily: {
         sansB: ['var(--noto-sans_KR-bold)'],
         sansM: ['var(--noto-sans_KR-medium)'],
@@ -20,6 +39,38 @@ module.exports = {
         container: '1140px',
         innerContainer: '644px',
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            pre: {
+              color: theme('colors.grey.1000'),
+              backgroundColor: theme('colors.grey.100'),
+            },
+            'pre code::before': {
+              content: '""',
+              'padding-left': 'unset',
+            },
+            'pre code::after': {
+              content: '""',
+              'padding-right': 'unset',
+            },
+            code: {
+              backgroundColor: theme('colors.grey.100'),
+              color: '#DD1144',
+              fontWeight: '400',
+              'border-radius': '0.25rem',
+            },
+            'code::before': {
+              content: '""',
+              'padding-left': '0.25rem',
+            },
+            'code::after': {
+              content: '""',
+              'padding-right': '0.25rem',
+            },
+          },
+        },
+      }),
     },
   },
   plugins: [require('@tailwindcss/typography')],
