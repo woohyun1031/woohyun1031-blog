1a30b32cb545bba919d1925951f3f7b6d49cf11f
KimWooHyun <ktkwhms3@gmail.com>
2023-05-10 16:49:38 +0900

    Feat: header, footer darkmode border color 변경

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -32,86 +32,28 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="sticky top-0 z-50 flex justify-center border-b-2 border-gray-200 py-3 px-6 backdrop-blur-md dark:border-gray-500">
-      <div className="mx-2 flex w-full max-w-header flex-wrap items-center justify-between">
-        <Link
-          href="/blog"
-          className="mr-6 flex flex-shrink-0 cursor-pointer items-center"
-        >
-          <span className="font-sansM text-xl text-blue-600">HEY</span>
-          <span className="font-sansM text-xl text-gray-900 dark:text-white">
-            DEV
-          </span>
-        </Link>
+    <header
+      className="sticky top-0 z-50 border-b-2 border-gray-200 backdrop-blur-md
+      dark:border-transparent         
+      dark:bg-gradient-to-r 
+    dark:from-cyan-500 
+    dark:to-blue-500"
+    >
+      <div className="flex justify-center py-3 px-6 duration-300 ease-in-out dark:bg-gray-900">
+        <div className="mx-2 flex w-full max-w-header flex-wrap items-center justify-between">
+          <Link
+            href="/blog"
+            className="mr-6 flex flex-shrink-0 cursor-pointer items-center"
+          >
+            <span className="font-sansM text-xl text-blue-600">HEY</span>
+            <span className="font-sansM text-xl text-gray-900 dark:text-white">
+              DEV
+            </span>
+          </Link>
 
-        <div className="flex lg:hidden">
-          <button
-            className="text-gray-800 dark:text-white"
-            onClick={() => changeTheme()}
-          >
-            {isDark ? (
-              <div>
-                <Image
-                  src="/sun.svg"
-                  alt="me"
-                  width="25"
-                  height="25"
-                  className="fill-current"
-                />
-              </div>
-            ) : (
-              <div>
-                <Image src="/moon.svg" alt="me" width="25" height="25" />
-              </div>
-            )}
-          </button>
-          <button
-            className="flex items-center rounded  px-3 py-2 text-gray-900  hover:text-blue-600"
-            onClick={() => setIsShow((prev) => !prev)}
-          >
-            {isShow ? (
-              <svg
-                className="fill-current dark:text-white"
-                viewBox="0 96 960 960"
-                height="20"
-                width="20"
-              >
-                <path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
-              </svg>
-            ) : (
-              <svg
-                className="fill-current dark:text-white"
-                viewBox="0 96 960 960"
-                height="20"
-                width="20"
-              >
-                <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
-              </svg>
-            )}
-          </button>
-        </div>
-        {isShow && (
-          <div className="w-full lg:hidden">
-            <div className="text-sm">
-              <Link
-                href="./resume"
-                className="mt-4 block cursor-pointer text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
-              >
-                채용
-              </Link>
-              <Link
-                href="/blog"
-                className="mt-4 block cursor-pointer text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
-              >
-                블로그
-              </Link>
-            </div>
-          </div>
-        )}
-        <div className="hidden w-full lg:flex lg:w-auto lg:items-center ">
-          <div className="flex text-sm lg:flex-grow">
+          <div className="flex lg:hidden">
             <button
-              className="mr-8 text-gray-800 dark:text-white"
+              className="text-gray-800 dark:text-white"
               onClick={() => changeTheme()}
             >
               {isDark ? (
@@ -130,22 +72,88 @@ const Header = () => {
                 </div>
               )}
             </button>
-            <button>
-              <Link
-                href="./resume"
-                className="mt-4 mr-8 block cursor-pointer  text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
-              >
-                채용
-              </Link>
+            <button
+              className="flex items-center rounded  px-3 py-2 text-gray-900  hover:text-blue-600"
+              onClick={() => setIsShow((prev) => !prev)}
+            >
+              {isShow ? (
+                <svg
+                  className="fill-current dark:text-white"
+                  viewBox="0 96 960 960"
+                  height="20"
+                  width="20"
+                >
+                  <path d="m249 849-42-42 231-231-231-231 42-42 231 231 231-231 42 42-231 231 231 231-42 42-231-231-231 231Z" />
+                </svg>
+              ) : (
+                <svg
+                  className="fill-current dark:text-white"
+                  viewBox="0 96 960 960"
+                  height="20"
+                  width="20"
+                >
+                  <path d="M120 816v-60h720v60H120Zm0-210v-60h720v60H120Zm0-210v-60h720v60H120Z" />
+                </svg>
+              )}
             </button>
-            <button>
-              <Link
-                href="/blog"
-                className="mt-4 mr-8 block cursor-pointer text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
+          </div>
+          {isShow && (
+            <div className="w-full lg:hidden">
+              <div className="text-sm">
+                <Link
+                  href="./resume"
+                  className="mt-4 block cursor-pointer text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
+                >
+                  ABOUT
+                </Link>
+                <Link
+                  href="/blog"
+                  className="mt-4 block cursor-pointer text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
+                >
+                  DEVLOG
+                </Link>
+              </div>
+            </div>
+          )}
+          <div className="hidden w-full lg:flex lg:w-auto lg:items-center ">
+            <div className="flex text-sm lg:flex-grow">
+              <button
+                className="mr-8 text-gray-800 dark:text-white"
+                onClick={() => changeTheme()}
               >
-                블로그
-              </Link>
-            </button>
+                {isDark ? (
+                  <div>
+                    <Image
+                      src="/sun.svg"
+                      alt="me"
+                      width="25"
+                      height="25"
+                      className="fill-current"
+                    />
+                  </div>
+                ) : (
+                  <div>
+                    <Image src="/moon.svg" alt="me" width="25" height="25" />
+                  </div>
+                )}
+              </button>
+              <button>
+                <Link
+                  href="./resume"
+                  className="mt-4 mr-8 block cursor-pointer  text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
+                >
+                  ABOUT
+                </Link>
+              </button>
+              <button>
+                <Link
+                  href="/blog"
+                  className="mt-4 mr-8 block cursor-pointer text-stone-600 hover:text-black dark:text-white lg:mt-0 lg:inline-block"
+                >
+                  DEVLOG
+                </Link>
+              </button>
+            </div>
           </div>
         </div>
       </div>
@@ -155,7 +163,15 @@ const Header = () => {
 
 const Footer = () => {
   return (
-    <footer className="mt-14 flex justify-center border-t-2 border-gray-200 p-6 backdrop-blur-md dark:border-gray-500"></footer>
+    <footer
+      className="mt-14 flex h-20 justify-center border-t-2 border-gray-200 backdrop-blur-md dark:border-gray-500 
+      dark:border-transparent         
+      dark:bg-gradient-to-r 
+    dark:from-cyan-500 
+    dark:to-blue-500"
+    >
+      <div className="w-full dark:bg-gray-900" />
+    </footer>
   );
 };
 
