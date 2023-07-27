f3c09e2dc91594ba84c48824eec1c3563754f603
KimWooHyun <ktkwhms3@gmail.com>
2023-07-27 19:43:06 +0900

    Feat: font size 수정

--- components/about/Description.tsx
+++ components/about/Description.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Description = ({ children }: { children: React.ReactNode }) => {
   return (
-    <span className="break-keep font-sansT text-sm text-gray-700 dark:text-gray-400 sm:text-base">
+    <span className="break-keep font-sansT text-sm text-gray-600 dark:text-gray-400 sm:text-base">
       {children}
     </span>
   );
--- components/about/SubTitle.tsx
+++ components/about/SubTitle.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SubTitle = ({ children }: { children: React.ReactNode }) => {
   return (
-    <span className="break-keep font-sansM text-sm text-gray-800 dark:text-gray-200 sm:text-lg">
+    <span className="break-keep font-sansB text-sm text-gray-800 dark:text-gray-200 sm:text-base">
       {children}
     </span>
   );
