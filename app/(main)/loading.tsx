91f5448836d815e2045700f8c5978e2098febbda
KimWooHyun <ktkwhms3@gmail.com>
2023-09-04 14:56:59 +0900

    Feat: loading 공통 컴포넌트 생성

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -1,11 +1,5 @@
+import LoadingComponent from '#components/Loading';
+
 export default function Loading() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <div className="flex items-center justify-center space-x-2">
-        <div className="w h-1 animate-pulse rounded-full bg-gray-500 dark:bg-gray-500"></div>
-        <div className="w h-1 animate-pulse rounded-full bg-gray-500 dark:bg-gray-500"></div>
-        <div className="w h-1 animate-pulse rounded-full bg-gray-500 dark:bg-gray-500"></div>
-      </div>
-    </div>
-  );
+  return <LoadingComponent />;
 }
