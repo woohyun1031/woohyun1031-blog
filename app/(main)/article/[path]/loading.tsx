51cbf3ea48b06ab4dd3cb0248f0df3099cb97142
KimWooHyun <ktkwhms3@gmail.com>
2024-01-01 21:27:20 +0900

    Feat: 아티클 무한 스크롤 loading 시 스켈레톤 ui 적용

--- app/(main)/article/[path]/loading.tsx
+++ app/(main)/article/[path]/loading.tsx
@@ -1,16 +1,5 @@
 import LoadingComponent from 'app/components/Loading';
 
 export default function Loading() {
-  return (
-    <div className="mt-36 flex w-full animate-pulse justify-center">
-      <div className="min-h-screen w-full max-w-innerContainer px-4">
-        <div className="flex flex-col items-center">
-          <div className="mb-3 h-10 w-full rounded-full bg-gray-200 px-4 opacity-40 dark:bg-gray-700"></div>
-          <div className="mb-3 h-10 w-full rounded-full bg-gray-200 px-4 opacity-40 dark:bg-gray-700"></div>
-          <div className="mb-6 h-10 w-full rounded-full bg-gray-200 px-4 opacity-40 dark:bg-gray-700"></div>
-          <div className="mb-24 h-5 w-2/4 rounded-full bg-gray-300 opacity-40 dark:bg-gray-600"></div>
-        </div>
-      </div>
-    </div>
-  );
+  return <LoadingComponent />;
 }
--- app/(main)/article/form.tsx
+++ app/(main)/article/form.tsx
@@ -124,6 +124,20 @@ export default function Form(props: {
               );
             },
           )}
+          {!!hasNextPage && (
+            <>
+              <div className="mt-24 flex animate-pulse flex-col items-start">
+                <div className="mb-4 h-10 w-full rounded-full bg-gray-300 opacity-40 dark:bg-gray-600"></div>
+                <div className="mb-4 h-5 w-3/4 rounded-full bg-gray-200 opacity-40 dark:bg-gray-700"></div>
+                <div className="h-4 w-1/3 rounded-full bg-gray-300 opacity-40 dark:bg-gray-700"></div>
+              </div>
+              <div className="mt-24 flex animate-pulse flex-col items-start">
+                <div className="mb-4 h-10 w-full rounded-full bg-gray-300 opacity-40 dark:bg-gray-600"></div>
+                <div className="mb-4 h-5 w-3/4 rounded-full bg-gray-200 opacity-40 dark:bg-gray-700"></div>
+                <div className="h-4 w-1/3 rounded-full bg-gray-300 opacity-40 dark:bg-gray-700"></div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
