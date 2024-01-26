9be4f8f80b58afde54466d85bcf21d5f4b7ff1db
KimWooHyun <ktkwhms3@gmail.com>
2024-01-26 14:28:05 +0900

    Feat: 404 error 페이지 테스트

---
+++ app/(main)/article/[path]/error.tsx
@@ -0,0 +1,30 @@
+'use client';
+
+import React from 'react';
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  React.useEffect(() => {
+    console.error('error:::', error);
+  }, [error]);
+
+  return (
+    <div className="flex w-full justify-center">
+      <div className="min-h-max w-full max-w-container px-4">
+        <div className="mt-36" />
+        <div className="flex flex-col items-center justify-center">
+          <p className="mb-9 font-sansT text-5xl text-gray-800 dark:text-gray-400">
+            Error 404 - Not Found
+          </p>
+          <p className="font-sansT">Sorry, This page does not exist :(</p>
+          {/* <button onClick={() => reset()}>Try again</button> */}
+        </div>
+      </div>
+    </div>
+  );
+}
