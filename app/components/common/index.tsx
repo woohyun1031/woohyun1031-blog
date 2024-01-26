4a7c31b0c5aba7f554a1d19563603e6d4823ebcf
KimWooHyun <ktkwhms3@gmail.com>
2024-01-26 13:20:00 +0900

    Feat: 스켈레톤 추가

--- app/components/common/index.tsx
+++ app/components/common/index.tsx
@@ -2,7 +2,15 @@ import Footer from './Footer';
 import GoogleAnalytics from './GoogleAnalytics';
 import Header from './Header';
 import ListTag from './ListTag';
-import LoadingComponent from './Loading';
+import { LoadingComponent, SkeletonComponent } from './Loading';
 import Tag from './Tag';
 
-export { Footer, GoogleAnalytics, Header, ListTag, LoadingComponent, Tag };
+export {
+  Footer,
+  GoogleAnalytics,
+  Header,
+  ListTag,
+  LoadingComponent,
+  Tag,
+  SkeletonComponent,
+};
