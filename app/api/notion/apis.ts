443c948d8c613b5c99f1b35e7c978878902b9860
KimWooHyun <ktkwhms3@gmail.com>
2024-03-22 22:24:56 +0900

    Fix: 라우트 핸들러 빌드 에러 수정
    fix: 라우트 핸들러 서버는 빌드타임엔 존재하지 않기 때문에 generateStaticParams와 함께 사용할 수 없다.
    chore: 불필요한 코드제거

--- app/api/notion/apis.ts
+++ app/api/notion/apis.ts
@@ -3,10 +3,23 @@ import { IPage } from '@apis/notion/notion';
 export const getNotionPage = async (
   id?: string,
 ): Promise<IPage | undefined> => {
-  if (!id) return;
-  const response = await fetch(
-    `${process.env.NEXT_API_URL}/api/notion/pages?id=${id}`,
-  );
-  const data = await response.json();
-  return data;
+  try {
+    if (!id) return;
+    const response = await fetch(
+      `${process.env.NOTION_PUBLIC_URL}/pages/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.NOTION_TOKEN}`,
+          Accept: 'application/json',
+          'Notion-Version': '2022-06-28',
+          'Content-Type': 'application/json',
+        },
+      },
+    );
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+    return Promise.reject(e);
+  }
 };
--- next.config.js
+++ next.config.js
@@ -9,27 +9,6 @@ var nextConfig = {
       },
     ];
   },
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule) =>
-      rule.test?.test?.('.svg'),
-    );
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/, // *.svg?url
-      },
-      {
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        resourceQuery: { not: /url/ }, // exclude if *.svg?url
-        use: ['@svgr/webpack'],
-      },
-    );
-    fileLoaderRule.exclude = /\.svg$/i;
-
-    return config;
-  },
   images: {
     domains: ['s3.us-west-2.amazonaws.com', 'toss.tech', 'www.notion.so'],
   },
