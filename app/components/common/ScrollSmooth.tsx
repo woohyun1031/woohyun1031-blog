d40d24cac554bfa1252cfbd36106b6e08b934a15
KimWooHyun <ktkwhms3@gmail.com>
2024-04-02 23:13:44 +0900

    Feat: ScrollSmooth 컴포넌트 추가
    feat: ScrollSmooth 컴포넌트 추가 하여 main 컨텐츠, footer 랩핑

---
+++ app/components/common/ScrollSmooth.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import React from 'react';
+
+export function ScrollSmooth({
+  children,
+}: {
+  children: React.ReactNode;
+}): React.ReactElement {
+  const scrollWrapperRef = React.useRef<HTMLDivElement>(null);
+  const offsetRef = React.useRef(0);
+  const speed = 0.05;
+
+  const scrollAnimate = () => {
+    offsetRef.current += (window.scrollY - offsetRef.current) * speed;
+    const scroll = `translate3d(0,${-offsetRef.current}px,0`;
+    if (scrollWrapperRef.current?.style) {
+      scrollWrapperRef.current.style.transform = scroll;
+      requestAnimationFrame(scrollAnimate);
+    }
+  };
+
+  React.useEffect(() => {
+    const requestId = requestAnimationFrame(scrollAnimate);
+    return () => cancelAnimationFrame(requestId);
+  }, []);
+
+  return (
+    <section className="h-screen">
+      <main ref={scrollWrapperRef}>{children}</main>{' '}
+    </section>
+  );
+}
