27a2bf69072be5ae7b43c28d39da0dce3fd790a2
KimWooHyun <ktkwhms3@gmail.com>
2024-04-06 15:44:11 +0900

    Refactor: ContributionGraph 구조 변경
    refactor: ContributionGraph sticky로 인한 bg 설정을 유동적으로 변경할 수 있게 수정

--- app/components/about/ContributionGraph.tsx
+++ app/components/about/ContributionGraph.tsx
@@ -35,67 +35,92 @@ export default function ContributionGraph({ username }: { username?: string }) {
           contributions in the last year
         </span>
       </div>
-      <div className="relative mt-3 w-full overflow-auto whitespace-nowrap">
-        <table className="table-fixed border-separate border-spacing-[3px] ">
-          <thead>
-            <tr className="h-[30px]">
-              <td className="sticky left-0 w-[28px] bg-white duration-300 ease-in-out dark:bg-black">
-                <span className="hidden">Day of Week</span>
-              </td>
-              {getMonthLabels(weeks).map(({ label: text, colSpan }) => (
-                <td colSpan={colSpan} className="pb-2">
-                  <span className="text-xs font-bold text-gray-700 duration-300 ease-in-out dark:text-gray-300">
-                    {text}
+      <div className="flex">
+        <div className="relative mt-3">
+          <table className="mb-7 table-fixed border-separate border-spacing-[3px]">
+            <thead>
+              <tr className="h-[30px]">
+                <td className="w-[28px] pb-2 duration-300 ease-in-out">
+                  <span className="invisible text-xs font-bold text-gray-700 duration-300 ease-in-out dark:text-gray-300">
+                    Week
                   </span>
                 </td>
-              ))}
-            </tr>
-          </thead>
+              </tr>
+            </thead>
+            <tbody>
+              {label.week.map((day, index) => {
+                const isOdd = Boolean(index % 2);
+                return (
+                  <tr key={day} className="table-row">
+                    <td className="table-cell min-w-[40px]  border border-transparent pr-2 text-right duration-300 ease-in-out">
+                      <span
+                        className={`align-middle text-xs font-bold text-gray-700 duration-300 ease-in-out  dark:text-gray-300 ${
+                          isOdd ? '' : 'invisible'
+                        }`}
+                      >
+                        {day}
+                      </span>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
 
-          <tbody>
-            {label.week.map((day, index) => {
-              const isOdd = Boolean(index % 2);
-              return (
-                <tr key={day} className="table-row">
-                  <td className="sticky left-0 table-cell min-w-[40px] bg-white pr-2 text-right duration-300 ease-in-out dark:bg-black">
+        <div className="relative mt-3 w-full overflow-auto whitespace-nowrap">
+          <table className="mb-7 table-fixed border-separate border-spacing-[3px]">
+            <thead>
+              <tr className="h-[30px]">
+                {getMonthLabels(weeks).map(({ label: text, colSpan }) => (
+                  <td colSpan={colSpan} className="pb-2">
                     <span className="text-xs font-bold text-gray-700 duration-300 ease-in-out dark:text-gray-300">
-                      {isOdd && day}
+                      {text}
                     </span>
                   </td>
-                  {weeks
-                    ? weeks
-                        .filter((week) => !!week.contributionDays?.[index])
-                        .map((week) => {
-                          const target = week.contributionDays[index];
-                          return (
-                            <td
-                              key={target.date}
-                              className="table-cell min-w-[28px] cursor-pointer rounded-sm border border-gray-300 text-center dark:border-gray-500"
-                            >
-                              <span
-                                className={`${
-                                  target.contributionCount
-                                    ? 'text-sm text-gray-700 dark:text-gray-50'
-                                    : 'text-xs text-gray-300 dark:text-gray-500'
-                                }`}
+                ))}
+              </tr>
+            </thead>
+
+            <tbody>
+              {label.week.map((day, index) => {
+                return (
+                  <tr key={day} className="table-row">
+                    {weeks
+                      ? weeks
+                          .filter((week) => !!week.contributionDays?.[index])
+                          .map((week) => {
+                            const target = week.contributionDays[index];
+                            return (
+                              <td
+                                key={target.date}
+                                className="table-cell min-w-[28px] cursor-pointer rounded-sm border border-gray-300 text-center dark:border-gray-500"
                               >
-                                {target.contributionCount}
-                              </span>
-                            </td>
-                          );
-                        })
-                    : [...new Array(7).fill(0)].map(() => (
-                        <td className="table-cell min-w-[28px] cursor-pointer rounded-sm border border-gray-300 text-center dark:border-gray-500">
-                          <span className="text-xs text-gray-300 dark:text-gray-500">
-                            0
-                          </span>
-                        </td>
-                      ))}
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
+                                <span
+                                  className={`${
+                                    target.contributionCount
+                                      ? 'text-sm text-gray-700 dark:text-gray-50'
+                                      : 'text-xs text-gray-300 dark:text-gray-500'
+                                  }`}
+                                >
+                                  {target.contributionCount}
+                                </span>
+                              </td>
+                            );
+                          })
+                      : [...new Array(7).fill(0)].map(() => (
+                          <td className="table-cell min-w-[28px] cursor-pointer rounded-sm border border-gray-300 text-center dark:border-gray-500">
+                            <span className="text-xs text-gray-300 dark:text-gray-500">
+                              0
+                            </span>
+                          </td>
+                        ))}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </>
   );
